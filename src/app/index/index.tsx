1afc30fb9c0f6f5db63594bbff021910b5618e8b
rodolfostark <rodolfojonatas@outlook.com>
2025-02-11 21:29:41 -0300

    :sparkles: feat: add app's header and config the directory structure

---
+++ src/app/index/index.tsx
@@ -0,0 +1,25 @@
+import { View, Image, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { styles } from "./styles"
+import { colors } from "@/styles/colors"
+
+export default function Index() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image 
+                    source={require("@/assets/logo.png")} 
+                    style={styles.logo} 
+                />
+                <TouchableOpacity>
+                    <MaterialIcons 
+                        name="add"
+                        size={32} 
+                        color={colors.green[300]} 
+                    />
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
