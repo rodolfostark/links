f4ebeecb509e04f9b7c1a6d6df542f103f2d0009
rodolfostark <rodolfojonatas@outlook.com>
2025-02-12 16:49:08 -0300

    :sparkles: feat: create menu options with modal to open and remove a link

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,11 @@
-import { View, Image, TouchableOpacity } from "react-native"
+import { View, Image, Text, TouchableOpacity, FlatList, Modal } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
+
+import { Link } from "@/components/link"
+import { Option } from "@/components/option"
 import { Categories } from "@/components/categories"
 
 export default function Index() {
@@ -22,6 +25,48 @@ export default function Index() {
                 </TouchableOpacity>
             </View>
             <Categories />
+            <FlatList 
+                data={["1", "2", "3", "4", "5", "6"]}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Link 
+                        name="Rocketseat" 
+                        url="https://app.rocketseat.com.br/"
+                        onDetails={() => console.log("Clicou!")} 
+                    />
+                )}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
+            />
+            <Modal transparent visible={true}>
+                <View style={styles.modal}>
+                    <View style={styles.modalContent}>
+                        <View style={styles.modalHeader}>
+                            <Text style={styles.modalCategory}>
+                                Curso
+                            </Text>
+                            <TouchableOpacity>
+                                <MaterialIcons 
+                                    name="close" 
+                                    size={20} 
+                                    color={colors.gray[400]} 
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={styles.modalLinkName}>
+                            Rocketseat
+                        </Text>
+                        <Text style={styles.modalUrl}>
+                            https://app.rocketseat.com.br/
+                        </Text>
+                        <View style={styles.modalFooter}>
+                            <Option name="Excluir" icon="delete" variant="secondary" />
+                            <Option name="Abrir" icon="language" />
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     )
 }
